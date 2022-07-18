a09b8d8e1164366d6eb31f8e12b87c6250551565
soleforna <informatica.ijo@gmail.com>
2022-07-18 12:14:05 -0300

    Proyecto Calculadora con JS

---
+++ js/Calculadora.js
@@ -0,0 +1,22 @@
+/* Creamos una clase donde se encuentran las operacioens básicas de una calculadora */
+class Calculadora{
+     /* La clase solo contiene métodos que resuleven los cálculos */
+     sumar(num1, num2){
+          return num1+num2
+     }
+
+     restar(num1, num2){
+          return num1-num2
+     }
+
+     multiplicar(num1, num2){
+          return num1*num2
+     }
+     
+     dividir(num1, num2){
+          return num1/num2
+     }
+
+
+
+}
